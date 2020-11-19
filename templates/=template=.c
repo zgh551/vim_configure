84609dd75746cfc3b3b1fd3422edbbff76b93b48
zgh551 <zgh_emial@163.com>
2020-11-19 11:25:02 +0800

    add file templates

---
+++ templates/=template=.c
@@ -0,0 +1,11 @@
+/*
+ * %FFILE%
+ * Copyright (C) %YEAR% %AUTHOR% <%EMAIL%>
+ * 
+ * Distributed under terms of the %LICENSE% license.
+ */
+
+#include "%FILE%.h"
+
+
+%HERE%
---
+++ templates/=template=.h
@@ -0,0 +1,13 @@
+/*
+ * %FFILE%
+ * Copyright (C) %YEAR% %AUTHOR% <%EMAIL%>
+ *
+ * Distributed under terms of the %LICENSE% license.
+ */
+
+#ifndef %GUARD%
+#define %GUARD%
+
+%HERE%
+
+#endif /* !%GUARD% */
